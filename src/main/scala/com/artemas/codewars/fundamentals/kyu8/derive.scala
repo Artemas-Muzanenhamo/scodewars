9410763ab5f969e592e09aa96354aecf42182226
Artemas-Muzanenhamo <29547780+Artemas-Muzanenhamo@users.noreply.github.com>
2024-09-11 14:50:24 +0100

    add derive solution

---
+++ src/main/scala/com/artemas/codewars/fundamentals/kyu8/derive.scala
@@ -0,0 +1,3 @@
+package com.artemas.codewars.fundamentals.kyu8
+
+def derive(coeff: Int, exp: Int): String = s"${coeff * exp}x^${exp - 1}"
---
+++ src/test/scala/com/artemas/codewars/fundamentals/kyu8/DeriveSpec.scala
@@ -0,0 +1,8 @@
+package com.artemas.codewars.fundamentals.kyu8
+
+class DeriveSpec extends org.scalatest.flatspec.AnyFlatSpec {
+  "derive" should "pass basic tests" in {
+    assert(derive(7, 8) == "56x^7", "for derive(7, 8)")
+    assert(derive(5, 9) == "45x^8", "for derive(5, 9)")
+  }
+}
